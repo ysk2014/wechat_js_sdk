a0c13eaef11529ee0f15e02d87aa7f003959ab81
yinshikai <yinshikai@taihe.com>
2016-06-14 18:34:45 +0800

    bug

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,19 @@ var router = express.Router();
 var cors = require('cors');
 var ctrl = require('../ctrl');
 
+app.all('*',function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+	res.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
+
+	if (req.method == 'OPTIONS') {
+		res.send(200); /让options请求快速返回/
+	}
+	else {
+		next();
+	}
+});
+
 app.use(cors());
 
 router.get('/', function(req, res, next) {
