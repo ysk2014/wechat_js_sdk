6ec32d75f3bdfa60897c89e583e621e6cd7cdd31
yinshikai <yinshikai@taihe.com>
2016-06-14 18:45:47 +0800

    bug

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,16 @@ var cors = require('cors');
 var ctrl = require('../ctrl');
 
 //设置跨域访问
-router.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1')
-    res.header("Content-Type", "application/json;charset=utf-8");
-    next();
-});
+// router.all('*', function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+//     res.header("X-Powered-By",' 3.2.1')
+//     res.header("Content-Type", "application/json;charset=utf-8");
+//     next();
+// });
 
-// router.use(cors());
+router.use(cors());
 
 router.get('/', function(req, res, next) {
 	res.send('启动成功');
