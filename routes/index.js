2cc3eec70facd27e2c37f23ea4d99966748da49e
yinshikai <yinshikai@taihe.com>
2016-06-14 18:58:00 +0800

    bug

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,11 @@ router.get('/', function(req, res, next) {
 	res.send('启动成功');
 });
 router.post('/', function(req, res, next) {
+	res.set({
+		"Access-Control-Allow-Origin": "*"
+		,"Access-Control-Allow-Methods": "POST,GET"
+		,"Access-Control-Allow-Credentials": "true"
+	});
   	ctrl(req, res);
 });
 
