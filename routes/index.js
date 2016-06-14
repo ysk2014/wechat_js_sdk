43b90b9320c74c844635f141e4a3a0d85cca1635
yinshikai <yinshikai@taihe.com>
2016-06-14 18:03:29 +0800

    bug

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,9 @@ var corsOptions = {
 	origin: 'http://wechat.yjshare.com'
 };
 
-
+router.get('/', function(req, res, next) {
+	res.send('启动成功');
+});
 router.post('/', cors(corsOptions), function(req, res, next) {
   	ctrl(req, res);
 });
