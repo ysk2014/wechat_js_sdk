4eef1620666844f98cedc0b4a05cd2a776605739
yinshikai <yinshikai@taihe.com>
2016-06-14 18:32:06 +0800

    bug

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,12 @@ var router = express.Router();
 var cors = require('cors');
 var ctrl = require('../ctrl');
 
-var corsOptions = {
-	origin: 'http://wechat.yjshare.com'
-};
+app.use(cors());
 
 router.get('/', function(req, res, next) {
 	res.send('启动成功');
 });
-router.post('/', cors(corsOptions), function(req, res, next) {
+router.post('/', function(req, res, next) {
   	ctrl(req, res);
 });
 
